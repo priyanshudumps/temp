5e54481ab721d31adb18eaf36acd7244eb410a65
adityaaggarwal12 <adityaaggarwal12339@gmail.com>
2024-06-23 00:32:15 +0530

    feat: coin metrics

--- src/jobs/dataCaching.job.js
+++ src/jobs/dataCaching.job.js
@@ -1,11 +1,15 @@
 const { cachingFunctions } = require("../services");
 
 const job = async () => {
+  // why is this outside promise.all?
+  // if all of these functions are called in parallel,
+  // then multiple connections pools to the database will be created
   await cachingFunctions.updateCachedCoinData();
   await Promise.all([
     cachingFunctions.updateCachedCoinLinksData(),
     cachingFunctions.updateCachedCoinScoreData(),
     cachingFunctions.updateCachedCoinDexData(),
+    cachingFunctions.updateCachedCoinMetricsData(),
   ]);
 };
 
--- src/services/index.js
+++ src/services/index.js
@@ -1,3 +1,4 @@
 exports.cachingFunctions = require("./cacheUpdate.service");
 exports.coinListService = require("./coinList.service");
 exports.dexDataService = require("./dexData.service");
+exports.coinMetricsService = require("./coinMetrics.service");
