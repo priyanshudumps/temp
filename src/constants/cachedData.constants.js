72c03c3e9804bf9e56134f71cfa55ba545882517
adityaaggarwal12 <adityaaggarwal12339@gmail.com>
2024-06-22 10:36:28 +0530

    feat: constants for caching db data locally

---
+++ src/constants/cachedData.constants.js
@@ -0,0 +1,5 @@
+exports.COINS = {};
+exports.COIN_LINKS = {};
+exports.COIN_SCORE = {};
+exports.COIN_DEX_METRICS = {};
+exports.COIN_METRICS = {};
---
+++ src/constants/index.js
@@ -0,0 +1,1 @@
+exports.cache = require("./cachedData.constants");
