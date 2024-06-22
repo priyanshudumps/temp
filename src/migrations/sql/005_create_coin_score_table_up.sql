c1c8f105136a5547dcdf061bb64d08fc701fee0c
adityaaggarwal12 <adityaaggarwal12339@gmail.com>
2024-06-22 10:39:23 +0530

    feat: updated coin score table

--- src/migrations/sql/005_create_coin_score_table_up.sql
+++ src/migrations/sql/005_create_coin_score_table_up.sql
@@ -1,12 +1,10 @@
 CREATE TABLE IF NOT EXISTS coin_score (
-        coin_id VARCHAR(255) REFERENCES coins(coin_id),
-        
+        coin_id VARCHAR(255) PRIMARY KEY REFERENCES coins(coin_id),
         score NUMERIC NULL,
-        is_banned BOOLEAN DEFAULT FALSE,
+        is_banned_panora BOOLEAN  NULL,
+        is_permissioned_hippo BOOLEAN  NULL,
         coin_market_cap_rank INTEGER NULL,
         geckoterminal_score NUMERIC NULL,
-
-
         created_at TIMESTAMPTZ DEFAULT NOW(),
         updated_at TIMESTAMPTZ DEFAULT NOW()
 )
