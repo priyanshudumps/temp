3663b2fdc58239f4fa64062f4bb04d585f5976db
adityaaggarwal12 <adityaaggarwal12339@gmail.com>
2024-07-05 19:07:34 +0530

    fix: format

--- src/services/currencyExchangeRates.service.js
+++ src/services/currencyExchangeRates.service.js
@@ -3,25 +3,28 @@ const Clients = require("./clients");
 const logger = require("../config/logger");
 
 const InsertOrUpdateCurrencyPricesData = async () => {
-    try {
-        const allCurrencyPrices = []
-        const allSupportesCurrencies = await Clients.exchangeRateClient.getAllSupportedCodes();
-        const allPricesUsd = await Clients.exchangeRateClient.getAllCurrenciesUsdPrices();
-        for(const currencyPair of allSupportesCurrencies['supported_codes']){
-            allCurrencyPrices.push({
-                currency_id: currencyPair[0],
-                country: currencyPair[1],
-                base_currency: "USD",
-                price: allPricesUsd['conversion_rates'][currencyPair[0]]
-            })
-        }
-        await methods.currencyPrices.addMultipleCurrencyPricesOrUpdate(allCurrencyPrices);
-
-    } catch (error) {
-        logger.error("Error while inserting currency prices data", error);
+  try {
+    const allCurrencyPrices = [];
+    const allSupportesCurrencies =
+      await Clients.exchangeRateClient.getAllSupportedCodes();
+    const allPricesUsd =
+      await Clients.exchangeRateClient.getAllCurrenciesUsdPrices();
+    for (const currencyPair of allSupportesCurrencies["supported_codes"]) {
+      allCurrencyPrices.push({
+        currency_id: currencyPair[0],
+        country: currencyPair[1],
+        base_currency: "USD",
+        price: allPricesUsd["conversion_rates"][currencyPair[0]],
+      });
     }
-}
+    await methods.currencyPrices.addMultipleCurrencyPricesOrUpdate(
+      allCurrencyPrices
+    );
+  } catch (error) {
+    logger.error("Error while inserting currency prices data", error);
+  }
+};
 
 module.exports = {
-    InsertOrUpdateCurrencyPricesData
-}
+  InsertOrUpdateCurrencyPricesData,
+};
