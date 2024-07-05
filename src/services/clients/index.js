2e136caba4e843a14e9ce14c2e59ce4e67e85411
adityaaggarwal12 <adityaaggarwal12339@gmail.com>
2024-07-05 19:04:45 +0530

    feat: exchange rate api clienty

--- src/services/clients/index.js
+++ src/services/clients/index.js
@@ -3,3 +3,4 @@ exports.panoraCoinClient = require("./panoraCoinList.client");
 exports.dexscreenerData = require("./dexscreener.client");
 exports.coinMarketcapClient = require("./coinmarketcap.client");
 exports.coinGeckoClient = require("./coingecko.client");
+exports.exchangeRateClient = require("./exchangeRateApi.client");
